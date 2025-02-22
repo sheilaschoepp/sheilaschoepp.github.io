08c349123e394b1298d8c199d40e6c36600f3db5
sheilaschoepp <sschoepp@ualberta.ca>
2025-02-22 18:18:49 +0000

    Deploying to gh-pages from @ sheilaschoepp/sheilaschoepp.github.io@918f72ccf16b716422a8d356d939bcec26ada631 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%65%69%6C%61%73%63%68%6F%65%70%70@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -513,7 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=uT_fklUAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
