2e6158d602ea9af33268f3551e21b1ac95197f80
sheilaschoepp <sschoepp@ualberta.ca>
2025-07-03 19:14:07 +0000

    Deploying to gh-pages from @ sheilaschoepp/sheilaschoepp.github.io@1058905074934e305dcf93f32fc5efa80128a5f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,35 +423,42 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%73%73%63%68%6F%65%70%70@%75%61%6C%62%65%72%74%61.%63%61", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=uT_fklUAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/sheilaschoepp", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/sheilaschoepp", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/sheilaschoepp", "_blank");
+        },
+      },{
+        id: 'social-notion_username',
+        title: 'Notion_username',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://sheilaschoepp.notion.site/", "_blank");
         },
       },{
       id: 'light-theme',
